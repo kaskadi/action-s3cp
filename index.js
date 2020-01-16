e7eab9b704c5f8abe0cefff4bcb0f00f80a1282a
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-16 16:06:55 +0100

    Removed action

--- index.js
+++ index.js
@@ -1,5 +1,84 @@
-const pj = require('./package.json')
-const fs = require('fs')
+const AWS = require('aws-sdk')
 const core = require('@actions/core')
+const fs = require('fs')
+const cwd = process.cwd()
+const package = require(`${cwd}/package.json`)
+console.log(package)
+const kaskadiOptions = package.kaskadi
 
-console.log(process.cwd())
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  region: 'eu-central-1',
+  accessKeyId: process.env.AWS_KEY_ID,
+  secretAccessKey: process.env.AWS_KEY_SECRET
+})
+
+kaskadiOptions['s3-push'].files.forEach(fileData => {
+  const file = fs.readFileSync(fileData.src)
+  const params = {
+    Body: file,
+    Bucket: 'kaskadi-public',
+    Key: resolvePath(fileData.dest)
+  }
+  console.log(params)
+  // s3.putObject(params).promise(console.log).catch(console.log)
+})
+
+function resolvePath (path) {
+  let parsedPath = parse(path)
+  parsedPath = parsedPath.map(token => {
+    if (token.type === 'part') {
+      return token.value
+    } else {
+      return resolver(token.value)
+    }
+  })
+  return parsedPath.join('')
+}
+
+function resolver (tokenValue) {
+  switch (tokenValue) {
+    case 'version':
+      return `v${package.version.split('.')[0]}/`
+  }
+}
+
+function parse (path) {
+  let state = 0
+  let result = []
+  let currentToken = ''
+  for (let i=0; i < path.length; i++) {
+    let currentChar = path.charAt(i)
+    switch (state) {
+      case 0:
+        if (currentChar === '{') {
+          state = 1
+          result.push({
+            type: 'part',
+            value: currentToken
+          })
+          currentToken = ''
+        } else {
+          currentToken += currentChar
+        }
+      break
+      case 1:
+        if (currentChar === '}') {
+          state = 0
+          result.push({
+            type: 'token',
+            value: currentToken
+          })
+          currentToken = ''
+        } else {
+          currentToken += currentChar
+        }
+      break
+    }
+  }
+  result.push({
+    type: 'part',
+    value: currentToken
+  })
+  return result
+}
