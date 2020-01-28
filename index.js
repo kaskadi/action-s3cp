a559c730603ba71876c917ba8bf723546e5cc499
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-28 17:07:27 +0100

    Added folder support

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mime = require('mime-types')
 const cwd = process.cwd()
 const package = require(`${cwd}/package.json`)
 const kaskadiOptions = package.kaskadi
+const bucket = 'kaskadi-public'
 
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
@@ -12,17 +13,44 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_KEY_SECRET
 })
 
-kaskadiOptions['s3-push'].files.forEach(fileData => {
-  const file = fs.readFileSync(fileData.src)
-  const type = mime.lookup(fileData.src)
+kaskadiOptions['s3-push'].files.forEach(upload)
+
+function upload (data) {
+  const fileStats = fs.lstatSync(data.src)
+  if (fileStats.isDirectory()) {
+    uploadFolder(data)
+  } else {
+    uploadFile(data)
+  }
+}
+
+function uploadFolder (data) {
+  // create folder
   const params = {
-    Body: file,
-    Bucket: 'kaskadi-public',
-    Key: resolvePath(fileData.dest),
-    ContentType: type
+    Body: '',
+    Bucket: bucket,
+    Key: resolvePath(data.dest)
   }
   s3.putObject(params).promise(console.log).catch(console.log)
-})
+  // for all files in the folder, repeat upload process
+  fs.readdirSync(data.src).forEach(file => {
+    const uploadData = {
+      src: `${data.src}/${file}`,
+      dest: `${data.dest}/${file}`
+    }
+    upload(uploadData)
+  })
+}
+
+function uploadFile (data) {
+  const params = {
+    Body: fs.readFileSync(data.src),
+    Bucket: bucket,
+    Key: resolvePath(data.dest),
+    ContentType: mime.lookup(data.src)
+  }
+  s3.putObject(params).promise(console.log).catch(console.log)
+}
 
 function resolvePath (path) {
   let branch = getCurrentBranchName()
