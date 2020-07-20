da8db339dbd8190ca57563f7d590d8bbef4df61e
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-20 13:04:39 +0200

    Hotfix: updated break condition for dependency

--- helpers/install-dependencies.js
+++ helpers/install-dependencies.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-  if (!process.env.GITHUB_ACTIONS || process.env.GITHUB_REPOSITORY === 'kaskadi/action-slscli') {
+  if (!process.env.GITHUB_ACTIONS || process.env.GITHUB_REPOSITORY === 'kaskadi/action-s3cp') {
     return
   }
   const { spawnSync } = require('child_process')
