17e9be93fdb6a93071b0b2ffe15db51b9c0ab332
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-26 13:02:32 +0200

    Updated run-action logic

--- test/helpers/run-action.js
+++ test/helpers/run-action.js
@@ -1,20 +1,28 @@
-const { spawnSync } = require('child_process')
+const { spawn } = require('child_process')
+const path = require('path')
 
-module.exports = (steps) => {
+module.exports = async (steps) => {
   for (const step of steps) {
-    runStep(step)
+    await runStep(step).catch(process.exit)
   }
 }
 
 function runStep (step) {
-  try {
-    console.log(`INFO: running action ${step} step...`)
-    console.log(`\n************ ${step.toUpperCase()} STEP OUTPUT START ************\n`)
-    spawnSync('node', [`../src/${step}`], { stdio: 'inherit' })
-    console.log(`\n************ ${step.toUpperCase()} STEP ACTION OUTPUT END ************\n`)
-  } catch (err) {
-    console.log(`ERROR: an error occured in ${step} step...`)
-    console.log(err)
-    process.exit()
-  }
+  return new Promise((resolve, reject) => {
+    console.log(`\n************ running action ${step} step ************\n`)
+    const proc = spawn('node', [path.join(__dirname, '../..', `src/${step}`)])
+    proc.stdout.on('data', log)
+    proc.stderr.on('data', log)
+    proc.on('error', console.log)
+    proc.on('exit', code => {
+      if (code !== 0) {
+        reject(new Error(code))
+      }
+      resolve(code)
+    })
+  })
+}
+
+function log (data) {
+  console.log(data.toString().trim())
 }
